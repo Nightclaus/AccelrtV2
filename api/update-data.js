b251546e5d64b49d9bf1f0b27d33d1741e465f28
Nightclaus <159602138+Nightclaus@users.noreply.github.com>
2025-06-06 19:56:43 +1000

    Auto Commit Message

--- api/update-data.js
+++ api/update-data.js
@@ -56,6 +56,7 @@ export default async function handler(req, res) {
   // }
 
   try {
+    console.log('Recieved');
     const incomingData = req.body; // Vercel automatically parses JSON body
 
     if (!incomingData || typeof incomingData !== 'object' || Object.keys(incomingData).length === 0) {
@@ -63,6 +64,7 @@ export default async function handler(req, res) {
     }
 
     const docRef = db.collection(COLLECTION_NAME).doc(DOCUMENT_ID);
+    console.log("Found Ref");
 
     // Set the document. This will overwrite the document if it exists,
     // or create it if it doesn't. Firestore stores the JS object as a map.
